e5b0c5b5894b78c32b68162e5f5cdba165ff3de2
Jiahua Chen <codemonkey8banana@gmail.com>
2014-03-25 18:40:28 -0700

    123

--- war/js/abc.js
+++ war/js/abc.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
-	$(".date").datepicker();
+	
+	loadCalendars();
+	$("#location").geocomplete();
 });
+
+function loadCalendars() {
+	
+	var nowTemp = new Date();
+	var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+	var checkin = $('#startdatepicker').datepicker({
+
+	    beforeShowDay: function (date) {
+	        return date.valueOf() >= now.valueOf();
+	    },
+	    autoclose: true
+
+	}).on('changeDate', function (ev) {
+	    if (ev.date.valueOf() > checkout.datepicker("getDate").valueOf() || !checkout.datepicker("getDate").valueOf()) {
+
+	        var newDate = new Date(ev.date);
+	        newDate.setDate(newDate.getDate() + 1);
+	        checkout.datepicker("update", newDate);
+
+	    }
+	    $('#enddatepicker')[0].focus();
+	});
+
+	var checkout = $('#enddatepicker').datepicker({
+	    beforeShowDay: function (date) {
+	        if (!checkin.datepicker("getDate").valueOf()) {
+	            return date.valueOf() >= new Date().valueOf();
+	        } else {
+	            return date.valueOf() > checkin.datepicker("getDate").valueOf();
+	        }
+	    },
+	    autoclose: true
+
+	}).on('changeDate', function (ev) {});
+	
+}
+
